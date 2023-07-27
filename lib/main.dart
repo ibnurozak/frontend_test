64bd4cc6c5bc469c3df3a7cc23e8e7552905cf73
ibnurozak <ibnurozakhii@gmail.com>
2023-07-27 18:33:29 +0700

    test case done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_test/authentication.dart/login.dart';
 
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
